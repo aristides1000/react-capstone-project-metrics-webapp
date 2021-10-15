6bebcd682b9d5db910ee27ec12350f07fa364088
Aristides Jose Molina Perez <aristides1000@gmail.com>
2021-10-14 21:16:44 -0400

    Finish Snapshot test

--- src/__test__/Country.test.js
+++ src/__test__/Country.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import Country from '../components/Country';
 
-test('The spinner appears in the Country view', () => {
-  const { container } = render(<Country />);
-  const linkElement = container.querySelector('.spinner');
-  expect(linkElement).toBeInTheDocument();
+describe('Country test', () => {
+  test('The spinner appears in the Country view', () => {
+    const { container } = render(<Country />);
+    const linkElement = container.querySelector('.spinner');
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('Test of Snapshot', () => {
+    const tree = renderer.create(<Country />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
